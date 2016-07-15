509762173fdb5d5cb0a1bb2e9a815962d17ba123
Armando-Ordorica <armandordorica@gmail.com>
2016-07-15 10:25:44 -0400

    right before changing the orders.php file from admin folder to back folder under templates

--- public/admin/index.php
+++ public/admin/index.php
@@ -25,6 +25,11 @@
                 if(($_SERVER['REQUEST_URI'] == "/ecom/public/admin/") || ($_SERVER['REQUEST_URI'] == "/ecom/public/admin/index.php")){
                include(TEMPLATE_BACK . "/admin_content.php");
                 }
+
+                if(isset($_GET['orders'])){
+                    include(TEMPLATE_BACK . "/orders.php");
+
+                }
                 ?>
                  <!-- FIRST ROW WITH PANELS -->
                 <?php echo $_SERVER['REQUEST_URI']; ?> 
