971818df755a4ebe389df5869a25372008847740
Armando-Ordorica <armandordorica@gmail.com>
2016-07-24 17:48:07 -0400

    showing dynamic categories in the select html element

--- resources/functions.php
+++ resources/functions.php
@@ -318,6 +318,24 @@ redirect("index.php?products");
 }
 
 
+}
+
+
+function show_categories_add_product_page(){ 
+
+$query = query("SELECT * FROM categories");
+confirm($query);
+ 
+while($row = fetch_array($query)) { 
+$categories_options = <<<DELIMETER
+<option value="{$row['cat_id']}">{$row['cat_title']}</option>
+DELIMETER;
+
+echo $categories_options;
+
+    }
+
+
 
 }
 
